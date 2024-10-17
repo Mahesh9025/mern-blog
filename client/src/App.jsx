127faa8481be4f15f616d52d3f4f03127761a597
maheshp90 <mahesh.ponnuru@devrabbit.com>
2024-10-17 23:57:49 +0530

    install react js and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-3xl text-red-500'>
+      App
+    </div>
+  )
+}
+
+export default App
