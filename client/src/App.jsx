06c777299545c3d87877d18a835af7ed43c6570e
maheshp90 <mahesh.ponnuru@devrabbit.com>
2024-10-18 00:24:05 +0530

    Create Pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Signin from './pages/signin'
+import Signup from './pages/Signup'
+import Projects from './pages/Projects'
 
 
 
@@ -7,9 +13,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='text-3xl text-red-500'>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/sigin' element={<Signin /> } />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
